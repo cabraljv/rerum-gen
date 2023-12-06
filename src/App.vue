bc7f6e3368378ab7923e03bc418bd73a294f5e92
cabraljv <me@cabraljv.dev>
2023-12-06 12:31:40 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <data-table :rows="rows" :columns="columns" />
+  </div>
+</template>
+
+<script>
+import DataTable from './components/DataTable.vue'
+
+export default {
+  components: { DataTable },
+  name: 'App',
+  data() {
+    return {
+      rows: [
+        { id: 1, name: 'John Doe', age: 30 },
+        { id: 2, name: 'Jane Doe', age: 25 },
+        { id: 3, name: 'Joe Doe', age: 20 }
+      ],
+      columns: ['id','name', 'age']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,26 @@
+<template>
+<table class="table">
+  <thead>
+    <tr>
+      <th v-for="column in columns" :key="column">{{ column }}</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="row in rows" :key="row.id">
+      <td v-for="column in columns" :key="column">{{ row[column] }}</td>
+    </tr>
+  </tbody>
+</table>
+  
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  props: ['rows', 'columns']
+}
+</script>
+
+<style>
+
+</style>
